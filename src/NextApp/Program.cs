fbab98202cb585ea56fe7ab609de210cff7151bd
kaatula <andrey.kuleshov@devexpress.com>
2015-09-28 13:08:02 +0300

    Adding console app

---
+++ src/NextApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NextApp
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Hello, Linux world");
+
+            Console.ForegroundColor = color;
+        }
+    }
+}
